5cae7fd1d70a9a80e7a11879a8cc1dbfc9c4005b
jihadraz13 <80213764+jihadraz13@users.noreply.github.com>
2021-12-06 23:56:27 +0700

    Add files via upload

---
+++ residentevil.java
@@ -0,0 +1,83 @@
+public class residentevil {
+  public static void main(String[] args) {
+    Human ucup = null;
+
+    // Disini Zombie bisa memiliki tipe data Human 
+    // karena Zombie sendiri juga adalah Human
+    Human bradVicker = new Zombie("Brad Vicker", 100);
+
+    Human rickGrimes = new Human("Rick Grimes", 100);
+    Monster nemesis = new Monster("Nemesis", 1000, "Im fast as hell");
+
+    // ucup bisa berubah menjadi Monster walau ucup itu tipe datanya Human
+    ucup = nemesis;
+
+    System.out.println(nemesis.getSuperPower());
+    System.out.println(ucup.getSuperPower());
+  }
+}
+
+class Human {
+  protected String name;
+  protected int energy;
+  protected String superPower = "I dont have any";
+
+  Human(String name, int energy) {
+    this.name = name;
+    this.energy = energy;
+  }
+
+  Human(String name, int energy, String superPower) {
+    this.name = name;
+    this.energy = energy;
+    this.superPower = superPower;
+  }
+  
+  public String getSuperPower() {
+    return this.superPower;
+  }
+
+  public String scream() {
+    return "Im a SuperHero";
+  }
+
+  public String run() {
+    return "Run Bitch, RUNNN";
+  }
+}
+
+class Zombie extends Human {
+  Zombie(String name, int energy) {
+    super(name, energy);
+  }
+
+  Zombie(String name, int energy, String superPower) {
+    super(name, energy, superPower);
+  }
+
+  @Override
+  public String scream() {
+    return "Im a Zombie";
+  }
+
+  @Override
+  public String run() {
+    return "Srreet, Srret";
+  }
+}
+
+class Monster extends Zombie{
+  Monster(String name, int energy, String superPower) {
+    super(name, energy, superPower);
+  }
+
+  @Override
+  public String scream() {
+    return "Im a Monster";
+  }
+
+  @Override
+  public String run() {
+    return "Wusssh";
+  }
+}
